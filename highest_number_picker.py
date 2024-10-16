1716b3f74eb6eb1c0eef2b961bc49b43c7a4d8a3
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-16 12:48:29 +0800

    done adding the users input to get 5 numbers

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -1,12 +1,10 @@
 #get 5 numbers from the user
-no1 = 15
-no2 = 12
-no3 = 13
-no4 = 14
-no5 = 15
-
-
-#have if statement that picks the highest number
+print("Give 5 numbers and I'll display the highest")
+number_1 = int(input("give the first number: "))
+number_2 = int(input("give the second number: "))
+number_3 = int(input("give the third number: "))
+number_4 = int(input("give the fourth number: "))
+number_5 = int(input("give the fifth number: "))
 
 #make a variable that holds the highest number
 
