63f555ab17ac1be11994c9dc374c3b969b2db773
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-16 12:49:33 +0800

    have a working highest number picker

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -7,7 +7,17 @@ number_4 = int(input("give the fourth number: "))
 number_5 = int(input("give the fifth number: "))
 
 #make a variable that holds the highest number
-
+highest_number = 0
 #compare number 1 - 5 with highest number variable
-
+if number_1 > highest_number:
+    highest_number = number_1
+if number_2 > highest_number:
+    highest_number = number_2
+if number_3 > highest_number:
+    highest_number = number_3
+if number_4 > highest_number:
+    highest_number = number_4
+if number_5 > highest_number:
+    highest_number = number_5
 #print the highest variable
+print(highest_number)
