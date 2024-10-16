f59b90ed5c92ea0d5d62c39aa4120df261c9a3dd
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-16 14:13:37 +0800

    added a while loop for the sake of input user error (unrelated)

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -1,10 +1,45 @@
 #get 5 numbers from the user
 print("Give 5 numbers and I'll display the highest")
-number_1 = int(input("give the first number: "))
-number_2 = int(input("give the second number: "))
-number_3 = int(input("give the third number: "))
-number_4 = int(input("give the fourth number: "))
-number_5 = int(input("give the fifth number: "))
+
+while True:
+    number_1 = input("give the first number: ")
+    if number_1.isdigit():
+        number_1 = int(number_1)
+        break
+    else:
+        print("please put a number")
+    
+while True:
+    number_2 = input("give the second number: ")
+    if number_2.isdigit():
+        number_2 = int(number_2)
+        break
+    else:
+        print("please put a number")
+
+while True:
+    number_3 = input("give the third number: ")
+    if number_3.isdigit():
+        number_3 = int(number_3)
+        break
+    else:
+        print("please put a number")
+        
+while True:
+    number_4 = input("give the fourth number: ")
+    if number_4.isdigit():
+        number_4 = int(number_4)
+        break
+    else:
+        print("please put a number")
+
+while True:
+    number_5 = input("give the fifth number: ")
+    if number_5.isdigit():
+        number_5 = int(number_5)
+        break
+    else:
+        print("please put a number")
 
 #make a variable that holds the highest number
 highest_number = number_1
@@ -19,5 +54,3 @@ if number_5 > highest_number:
     highest_number = number_5
 #print the highest variable
 print(highest_number)
-
-#error when user inputting other things than integers
