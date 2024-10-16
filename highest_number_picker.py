89566f12d982390aaec3ae189573543720674224
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-16 12:41:39 +0800

    done pseudo code

---
+++ highest_number_picker.py
@@ -0,0 +1,15 @@
+#get 5 numbers from the user
+no1 = 15
+no2 = 12
+no3 = 13
+no4 = 14
+no5 = 15
+
+
+#have if statement that picks the highest number
+
+#make a variable that holds the highest number
+
+#compare number 1 - 5 with highest number variable
+
+#print the highest variable
