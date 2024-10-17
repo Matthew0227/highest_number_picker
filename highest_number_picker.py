fa83916866958be0c7c33b1e7875dcd62255f8b9
Matthew0227 <matthewjaveniar2@gmail.com>
2024-10-18 00:12:59 +0800

    have a working function to select the highest number

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -46,17 +46,34 @@ def highest_picker(highest_number_placeholder, num1_placeholder, num2_placeholde
 
 
 #compare num1 to num2, 3, 4, 5
-#make a temp list that holds the highest value
-#compare the highest value to num1,2,3,4,5 to check if there's equal value
-#push the equal value to the temp list
- if number_1 > number_2:
-    if number_1 > number_2:
+  if number_1 > number_2:
         if number_1 > number_3:
             if number_1 > number_4:
                 if number_1 > number_5:
                     highest_number_placeholder.append(number_1)
+  if number_2 > number_1:
+        if number_2 > number_3:
+            if number_2 > number_4:
+                if number_2 > number_5:
+                    highest_number_placeholder.append(number_2)
+  if number_3 > number_1:
+        if number_3 > number_2:
+            if number_3 > number_4:
+                if number_3 > number_5:
+                    highest_number_placeholder.append(number_3)
+  if number_4 > number_1:
+        if number_4 > number_2:
+            if number_4 > number_3:
+                if number_4 > number_5:
+                    highest_number_placeholder.append(number_4)
+  if number_5 > number_1:
+        if number_5 > number_2:
+            if number_5 > number_3:
+                if number_5 > number_4:
+                    highest_number_placeholder.append(number_5)
 
-        
+#compare the highest value to num1,2,3,4,5 to check if there's equal value
+#push the equal value to the highest_number list
 
 #make a variable that will hold the highest numbers
 highest_number = []
@@ -71,4 +88,4 @@ highest_num = highest_picker(highest_number, number_1, number_2, number_3, numbe
 print(highest_number)
 #the code will display the highest number even if there is two or more same values
 
-#Fixed by mistakingly printing the function
+#removing the temp variable as it seems not to be necessary
